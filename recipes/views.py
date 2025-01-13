b67bc83ff2981628e74d68d756bc51b3ca905503
MikhaSverdlovski <shirinkin.mikhail1992@gmail.com>
2025-01-13 12:22:47 +0300

    reciepes added

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from django.shortcuts import render, get_object_or_404
+from .models import Recipe
+import random
+
+def home(request):
+    recipes = Recipe.objects.all()
+    random_recipes = random.sample(list(recipes), min(len(recipes), 5))
+    return render(request, 'recipes/home.html', {'recipes': random_recipes})
+
+
+def recipe_detail(request, recipe_id):
+    recipe = get_object_or_404(Recipe, id=recipe_id)
+    return render(request, 'recipes/recipe_detail.html', {'recipe': recipe})
